e4fb66391306997c2a3fa31e32f6bb147a6f6a28
Sander Constantin <sander.constantin@student.kdg.be>
2024-07-09 01:14:05 +0200

    Extra test

--- Backend/Test/Scrapers/Carrefour/DetailTests.cs
+++ Backend/Test/Scrapers/Carrefour/DetailTests.cs
@@ -14,6 +14,15 @@ public class DetailTests
         item.Price.Should().BeGreaterThan(0);
         item.Details.Title.Should().NotBeEmpty();
         item.Details.Image.Should().NotBeEmpty();
-        item.Details.Description.Should().NotBeEmpty();
+    }
+    
+    [Test]
+    public async Task Chips()
+    {
+        var scraper = new CarrefourDetailScraper();
+        var item = await scraper.Scrape("https://www.carrefour.be/nl/original-hartige-snack-165-g/07090377.html");
+        item.Price.Should().BeGreaterThan(0);
+        item.Details.Title.Should().NotBeEmpty();
+        item.Details.Image.Should().NotBeEmpty();
     }
 }
