896263ef03e5dbb8d6938a63d92d62414538c0e6
Sander Constantin <58855319+SanderCC@users.noreply.github.com>
2024-07-09 15:28:55 +0200

    Update ScraperHttpClient.cs

--- Backend/Scrapers/Helpers/ScraperHttpClient.cs
+++ Backend/Scrapers/Helpers/ScraperHttpClient.cs
@@ -1,18 +1,30 @@
-﻿namespace Scrapers.Helpers;
+using System.Net;
+using System.Net.Http;
+
+namespace Scrapers.Helpers;
 
 public static class ScraperHttpClient
 {
+    private static HttpClient _client = null;
+    
     public static HttpClient Get()
     {
-        var client = new HttpClient();
-        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0");
-        client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
-        client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.9,en-US;q=0.8,nl;q=0.7");
-        client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform", "\"Windows\"");
-        client.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
-        client.DefaultRequestHeaders.Add("Sec-Ch-Ua", "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"");
-        client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
-        client.DefaultRequestHeaders.Add("Sec-Fetch-User", "?1");
-        return client;
+        if (_client != null) return _client;
+        var handler = new HttpClientHandler
+        {
+            AllowAutoRedirect = true,
+            UseCookies = true,
+            CookieContainer = new CookieContainer()
+        };
+        var client = new HttpClient(handler);
+        _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0");
+        _client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+        _client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.9,en-US;q=0.8,nl;q=0.7");
+        _client.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform", "\"Windows\"");
+        _client.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
+        _client.DefaultRequestHeaders.Add("Sec-Ch-Ua", "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"");
+        _client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
+        _client.DefaultRequestHeaders.Add("Sec-Fetch-User", "?1");
+        return _client;
     }
 }
