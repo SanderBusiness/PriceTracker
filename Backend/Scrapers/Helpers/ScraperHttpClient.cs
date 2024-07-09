0489f24e17636fbdf6abf4f0bdc5dfb5afce0e70
Sander Constantin <58855319+SanderCC@users.noreply.github.com>
2024-07-09 15:30:14 +0200

    Update ScraperHttpClient.cs

--- Backend/Scrapers/Helpers/ScraperHttpClient.cs
+++ Backend/Scrapers/Helpers/ScraperHttpClient.cs
@@ -16,7 +16,7 @@ public static class ScraperHttpClient
             UseCookies = true,
             CookieContainer = new CookieContainer()
         };
-        var client = new HttpClient(handler);
+        _client = new HttpClient(handler);
         _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0");
         _client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
         _client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.9,en-US;q=0.8,nl;q=0.7");
