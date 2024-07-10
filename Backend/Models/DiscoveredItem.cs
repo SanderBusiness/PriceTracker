ebb909dd8f1791b358afe654bce87cf3877abeaf
Sander Constantin <sconstantin@3p.eu>
2024-07-10 13:52:37 +0200

    No detail scrapers

--- Backend/Models/DiscoveredItem.cs
+++ Backend/Models/DiscoveredItem.cs
@@ -7,4 +7,6 @@ public class DiscoveredItem
     public Shop Shop { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public decimal Price { get; set; } = -1;
 }
--- Backend/Test/Scrapers/Carrefour/DiscoveryTests.cs
+++ Backend/Test/Scrapers/Carrefour/DiscoveryTests.cs
@@ -7,19 +7,17 @@ namespace Test.Scrapers.Carrefour;
 [TestFixture]
 public class DiscoveryTests
 {
-    private static HttpClient Client => ScraperHttpClient.Get();
-    
     [Test]
     public async Task SomeResults()
     {
-        var scraper = new CarrefourDiscoveryScraper(Client);
+        var scraper = new CarrefourScraper();
         var items = await scraper.Discover("banana");
         items.Count.Should().BeGreaterThan(0);
     }
     [Test]
     public async Task NoResults()
     {
-        var scraper = new CarrefourDiscoveryScraper(Client);
+        var scraper = new CarrefourScraper();
         var items = await scraper.Discover("____");
         items.Count.Should().Be(0);
     }
