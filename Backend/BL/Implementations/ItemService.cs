8b5cf1a9d32a519f6294901b5599620fb44adfeb
Sander Constantin <sconstantin@3p.eu>
2024-07-10 14:21:07 +0200

    Only scrape if 0 results

--- Backend/BL/Implementations/ItemService.cs
+++ Backend/BL/Implementations/ItemService.cs
@@ -2,23 +2,28 @@
 using DAL.EF;
 using Domain.Items;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BL.Implementations;
 
-public class ItemService(IScraperService scraperService, ApplicationContext db) : IItemService
+public class ItemService(IScraperService scraperService, ApplicationContext db, ILogger<ItemService> logger) : IItemService
 {
     public async Task<List<Item>> GetFromSearch(string searchQuery)
     {
         var count = await GetItemSearchQuery(searchQuery).CountAsync();
-        if (count < 5)
+        logger.LogInformation("Found {Count} items for search: {Query}", count, searchQuery);
+        if (count == 0)
             await scraperService.Discover(searchQuery);
         return await GetItemSearchQuery(searchQuery).ToListAsync();
     }
 
     private IQueryable<Item> GetItemSearchQuery(string searchQuery)
     {
+        var query = searchQuery.ToLower();
         return db.Items
             .Include(e => e.PriceHistory)
-            .Where(e => e.SearchQueries.Any(q => q.Search.ToLower().Contains(searchQuery.ToLower())));
+            .Where(e => e.SearchQueries
+                .Any(q => q.Search.ToLower().Contains(query)
+                || q.Item.Title.ToLower().Contains(query)));
     }
 }
